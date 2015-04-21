920cfd6767a9891f2b34bcbbe282b7cfa065b3c4
MichaelSamteladze <mike.samteladze@gmail.com>
2015-04-21 11:39:24 +0400

    Stage 1 Finish

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keyString = "012345678901234567890123";
+            var keyBytes = Encoding.UTF8.GetBytes(keyString);
+            var mac = new MACTripleDES(keyBytes);
+            var data = "please authenticate me example number one oh one point seven niner";
+            var macResult = mac.ComputeHash(Encoding.UTF8.GetBytes(data));            
+            Console.WriteLine(BitConverter.ToString(macResult));
+
+            var d = mac.(macResult);
+            Console.WriteLine(BitConverter.ToString(d));
+        }
+    }
+}
